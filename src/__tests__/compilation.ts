db16be9d73040c9d35b907eb936d9ae518808632
david <dcrbrown@outlook.com>
2019-07-04 22:27:31 +0100

    Add basic code replacements

---
+++ src/__tests__/compilation.ts
@@ -0,0 +1,58 @@
+import {compile, run} from "../compilation";
+
+describe('compile', () => {
+    it('can compile a simple program', () => {
+        const program = `
+            console.log('Hello World');
+        `;
+        compile(program);
+    });
+});
+
+describe('run', () => {
+    describe('replacing globals', () => {
+        it('can run', () => {
+            const program = `
+            console.log('Hello World');
+        `;
+
+            const replacements = {
+                console: {
+                    log: jest.fn()
+                }
+            };
+
+            run(program, replacements);
+            expect(replacements.console.log).toHaveBeenCalled();
+        });
+
+        it('replaces all console statements', () => {
+            const consoleLog = jest.spyOn(console, 'log');
+            const consoleWarn = jest.spyOn(console, 'warn');
+            const consoleError = jest.spyOn(console, 'error');
+
+            const program = `
+                console.log('Hello World');
+                console.warn('Hello World');
+                console.error('Hello World');
+            `;
+
+            const replacements = {
+                console: {
+                    log: jest.fn(),
+                    warn: jest.fn(),
+                    error: jest.fn()
+                }
+            };
+
+            run(program, replacements);
+            expect(consoleLog).not.toHaveBeenCalled();
+            expect(consoleWarn).not.toHaveBeenCalled();
+            expect(consoleError).not.toHaveBeenCalled();
+
+            expect(replacements.console.log).toHaveBeenCalledWith('Hello World');
+            expect(replacements.console.warn).toHaveBeenCalledWith('Hello World');
+            expect(replacements.console.error).toHaveBeenCalledWith('Hello World');
+        });
+    });
+});
--- src/compilation.ts
+++ src/compilation.ts
@@ -1,3 +1,56 @@
-export const compile = (program) => {
+import {parse} from '@babel/parser';
+import traverse, {NodePath} from '@babel/traverse';
+import generate from '@babel/generator';
+import * as t from '@babel/types';
+import {File, Identifier} from '@babel/types';
 
+export const compile = (program: string) => {
+    return parse(program, {
+        sourceType: "unambiguous",
+        // ranges: true
+    });
+};
+
+type GlobalReplacements = {
+    [key: string]: any;
+}
+
+const computeWindowPropertyName = (property: string) => {
+    return `__codeEditor__${property}`;
+};
+
+const replaceGlobal = (ast: File, globalReplacements: GlobalReplacements) => {
+    const visitor = {
+        Identifier: (path: NodePath<Identifier>) => {
+            if (
+                Object.keys(globalReplacements).includes(path.node.name)
+            ) {
+                path.replaceWith(t.identifier(computeWindowPropertyName(path.node.name)));
+            }
+        }
+    };
+
+    traverse(ast, visitor);
+
+    return ast;
+};
+
+const addPropsToWindow = (globalReplacements: GlobalReplacements) => {
+    Object.entries(globalReplacements).forEach(([key, value]) => {
+        if (!(key in window)) {
+            throw new Error(`key: ${key} is not a valid window property`);
+        }
+        (window as any)[computeWindowPropertyName(key)] = value;
+    });
+};
+
+export const run = (program: string, globalReplacements: GlobalReplacements) => {
+    const ast = parse(program);
+
+    const astReplaced = replaceGlobal(ast, globalReplacements);
+    const finalProgram = generate(astReplaced).code;
+
+    addPropsToWindow(globalReplacements);
+    // eslint-disable-next-line no-eval
+    eval(finalProgram);
 };
